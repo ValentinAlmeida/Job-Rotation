4bb1e50b3e7a4de4d0836d52b7fdf12c8f98bb90
Valentin <valentinfalmeida@hotmail.com>
2022-07-07 19:06:45 -0300

    Ready

---
+++ 2/src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class App {
+    
+public static void main(String[] args) {
+    Scanner scan= new Scanner(System.in);
+    System.out.println("Escolha um valor");
+    int valor = scan.nextInt();
+    boolean certo = false;
+
+    for (int i = 0; i <= valor; i++) {
+        if (fibonacci(i) == valor){
+            System.out.println("O valor pertence a sequencia de fibonacci");
+            certo = true;
+        }
+    }
+
+    if(certo == false){
+        System.out.println("O valor não pertence a sequencia de fibonacci");
+    }
+}
+
+public static long fibonacci(int n) {
+    return n < 2 ? n : fibonacci(n - 2) + fibonacci(n - 1);
+}
+}
---
+++ 5/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Escolha uma palavra");
+        String palavra = scan.next();
+        System.out.println(reverse(palavra));
+    }
+    static String reverse(String original) {
+        String reversed = "";
+        for (int i = original.length() - 1;  0 <= i; i--) {
+            reversed += original.charAt(i);
+        }
+        return reversed;
+    }
+}
